ccc0d43555bf4e9434abe42cde59868d90799d4c
eqtstv <38236287+eqtstv@users.noreply.github.com>
2020-03-12 12:04:40 +0100

    add merge_sort

--- lab1.py
+++ lab1.py
@@ -11,17 +11,55 @@ def partition(array, start, end):
     array[low_index + 1], array[end] = array[end], array[low_index + 1] 
     return low_index + 1
 
-def quickSort(array, start, end):
+def quick_sort(array, start, end):
     if start < end: 
         pivot = partition(array, start, end)
-        quickSort(array, start, pivot - 1) 
-        quickSort(array, pivot + 1, end) 
+        quick_sort(array, start, pivot - 1) 
+        quick_sort(array, pivot + 1, end) 
+
+def merge_sort(array): 
+    if len(array) > 1: 
+        mid_index = len(array) // 2
+        left_side = array[:mid_index]
+        right_side = array[mid_index:]
+  
+        merge_sort(left_side)
+        merge_sort(right_side)
+
+        left_index = right_index = array_index = 0
+
+        while left_index < len(left_side) and right_index < len(right_side): 
+            if left_side[left_index] < right_side[right_index]: 
+                array[array_index] = left_side[left_index] 
+                left_index += 1
+            else: 
+                array[array_index] = right_side[right_index] 
+                right_index += 1
+            array_index += 1
+
+        while left_index < len(left_side): 
+            array[array_index] = left_side[left_index] 
+            left_index += 1
+            array_index += 1
+          
+        while right_index < len(right_side): 
+            array[array_index] = right_side[right_index] 
+            right_index += 1
+            array_index += 1
+
 
 if __name__ == "__main__":
-    array_to_sort = random.sample(range(100000), 100000)
+    array_to_sort_1 = random.sample(range(100000), 100000)
+    array_to_sort_2 = random.sample(range(100000), 100000)
 
     start1 = timer()
-    quickSort(array_to_sort, 0, len(array_to_sort)-1) 
+    quick_sort(array_to_sort_1, 0, len(array_to_sort_1)-1) 
     end1 = timer()
 
+    start2 = timer()
+    merge_sort(array_to_sort_2)
+    end2 = timer()
+    
     print("Quick sort: {} seconds".format(round(end1-start1, 4)))
+    print("Merge sort: {} seconds".format(round(end2-start2, 4)))
+    
