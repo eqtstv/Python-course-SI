b69551f6457ca48aba5b9f16bada673a71e93297
eqtstv <38236287+eqtstv@users.noreply.github.com>
2020-05-02 23:27:45 +0200

    sell difference, final commit

--- l5.py
+++ l5.py
@@ -4,39 +4,33 @@ import time
 
 def create_dataframe():
     df = pd.DataFrame(index=['BTC', 'ETH', 'Dash', 'LTC', 'LSK'])
-    df['Current Buy'] = [0 for i in range(5)]
-    df['Current Sell'] = [0 for i in range(5)]
-    df['Min Buy'] = [float("inf") for i in range(5)]
-    df['Max Sell'] = [0 for i in range(5)]
+    df['Current Price'] = [0 for i in range(5)]
+    df['Min Price'] = [float("inf") for i in range(5)]
+    df['Max Price'] = [0 for i in range(5)]
     df['Max Potential Profit [%]'] = [0 for i in range(5)]
     return df
 
 def update_prices(df, investment = float("inf")):
-    btc = requests.get('https://bitbay.net/API/Public/btcusd/orderbook.json').json()
-    eth = requests.get('https://bitbay.net/API/Public/ethusd/orderbook.json').json()
-    dash = requests.get('https://bitbay.net/API/Public/dashusd/orderbook.json').json()
-    ltc = requests.get('https://bitbay.net/API/Public/ltcusd/orderbook.json').json()
-    lsk = requests.get('https://bitbay.net/API/Public/lskusd/orderbook.json').json()
+    btc = requests.get('https://bitbay.net/API/Public/btcusd/orderbook.json').json()['bids']
+    eth = requests.get('https://bitbay.net/API/Public/ethusd/orderbook.json').json()['bids']
+    dash = requests.get('https://bitbay.net/API/Public/dashusd/orderbook.json').json()['bids']
+    ltc = requests.get('https://bitbay.net/API/Public/ltcusd/orderbook.json').json()['bids']
+    lsk = requests.get('https://bitbay.net/API/Public/lskusd/orderbook.json').json()['bids']
 
-    best_sell = []
-    best_buy = []
-    
-    for currency in [btc, eth, dash, ltc, lsk]:
-        best_sell.append((next(bid[0] for bid in currency['bids'] if bid[1] < investment / bid[0])))
-        best_buy.append((next(bid[0] for bid in currency['asks'] if bid[1] < investment / bid[0])))
+    best_prices_vol = []
+    for curr in [btc, eth, dash, ltc, lsk]:
+        best_prices_vol.append((next(bid[0] for bid in curr if bid[1] < investment / bid[0])))
 
-    df['Current Sell'] = best_sell
-    df['Current Buy'] = best_buy
-    df['Min Buy'] = df[['Current Buy', 'Min Buy']].min(axis=1)
-    df['Max Sell'] = df[['Current Sell', 'Max Sell']].max(axis=1)
-    df['Max Potential Profit [%]'] = round((df['Max Sell'] - df['Min Buy'])*100 / df['Min Buy'], 6)
+    df['Current Price'] = best_prices_vol
+    df['Min Price'] = df[['Current Price', 'Min Price']].min(axis=1)
+    df['Max Price'] = df[['Current Price', 'Max Price']].max(axis=1)
+    df['Max Potential Profit [%]'] = round((df['Max Price'] - df['Min Price'])*100 / df['Min Price'], 6)
     
 
 df = create_dataframe()
 while True:
-    update_prices(df)
-    #print(df.sort_values(by=['Max Potential Profit [%]'], ascending=False))
+    update_prices(df, 5000)
     print(df.loc[:, ['Max Potential Profit [%]']].sort_values(by=['Max Potential Profit [%]'], ascending=False))
     print('')
     print('-'*30)
-    time.sleep(2)
+    time.sleep(300)
